3a0ee80d51e037453c4f81e2c36c9ee99b2bc884
Dave Morschhauser <gitlab@netsavvy.com>
2017-09-22 12:38:49 -0400

    Initial project creation, with a nod to K&R

---
+++ KandR_HelloWorld.cpp
@@ -0,0 +1,15 @@
+// KandR_HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	/* With a nod to Kernighan and Ritchie
+	 */
+	printf("Hello World!\n");
+
+	return 0;
+}
+
